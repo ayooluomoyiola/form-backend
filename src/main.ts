1c6851fcb07bba96d69fc5df78a135f69545fde1
Ayoolu Omoyiola <ayooluomoyiola@gmail.com>
2025-07-23 09:46:25 +0100

    "enable cors"

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4000);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST',
+    credentials: true,
+  });
 }
 bootstrap();
