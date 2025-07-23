69ff45943e7d0bdf959b5eeb7b1efd58f9ef805d
Ayoolu Omoyiola <ayooluomoyiola@gmail.com>
2025-07-23 11:39:22 +0100

    feat: get all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post('create')
-    create(@Body() dto: CreateUserDto ) {
-        return this.userService.createUser(dto)
-    }
+  @Post('create')
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+
+  @Get('all')
+  get() {
+    return this.userService.getUsers();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { db } from 'src/db/drizzle-client';
-import {usersTable} from '../db/schema'
+import { usersTable } from '../db/schema';
 import { CreateUserDto } from './dto';
 
 @Injectable()
 export class UserService {
-    async createUser(dto: CreateUserDto) {
-        const result = await db.insert(usersTable).values(dto).returning()
-        return result[0]
+  async createUser(dto: CreateUserDto) {
+    const result = await db.insert(usersTable).values(dto).returning();
+    return result[0];
+  }
+
+  async getUsers() {
+    try {
+      const result = await db.query.usersTable.findMany();
+      return result;
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to fetch users');
     }
+  }
 }
- 
