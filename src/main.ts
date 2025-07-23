d2ce3aa979cb75237240399bdb4f863c8eaffd8a
Ayoolu Omoyiola <ayooluomoyiola@gmail.com>
2025-07-23 11:51:19 +0100

    cors changes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://form-app-liard-phi.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
